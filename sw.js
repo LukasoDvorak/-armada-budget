f5f641ab4c1c78cc43f7da290d97147d22cc2334
Lukas Dvorak <lukas.dvo@gmail.com>
2026-04-18 19:57:41 +0200

    b10: kbd nav bar always visible on input focus (iPad fix)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'armada-budget-b9';
+const CACHE = 'armada-budget-b10';
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -11,7 +11,7 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k \!== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
